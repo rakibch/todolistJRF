61ef0d7d3d560eab191d6efe1f6bd0b11acb87ec
rakibch <rakib9204@gmail.com>
2022-11-19 11:23:15 +0600

    css design modeling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,51 @@
-<?php
-echo 'hi';
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>TodList</title>
+<link rel="stylesheet" href="{{asset('/css/main.css')}}">
+</head>
+<body>
+<div class="container">
+  <form action="/action_page.php">
+    <div class="row">
+            <div class="col-25">
+                <h2>TO DO LIST</h2>
+            </div>
+    </div>
+    <div class="row">
+            <div class="col-25">
+            <label for="lname">Last Name</label>
+            </div>
+            <div class="col-75">
+            <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+            </div>
+    </div>
+    <div class="row">
+            <div class="col-25">
+                <label for="country">Country</label>
+            </div>
+            <div class="col-75">
+                <select id="country" name="country">
+                    <option value="australia">Australia</option>
+                    <option value="canada">Canada</option>
+                    <option value="usa">USA</option>
+                </select>
+            </div>
+    </div>
+    <div class="row">
+        <div class="col-25">
+        <label for="subject">Subject</label>
+        </div>
+        <div class="col-75">
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+        </div>
+    </div>
+  <br>
+    <div class="row">
+        <input type="submit" value="Submit">
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
